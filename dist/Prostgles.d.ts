6780e7c33f7480eb8012da96d86e2ca1ba13fce5
prostgles <prostgles@protonmail.com>
2020-09-11 19:59:53 +0100

    fix publishMethods

--- dist/Prostgles.d.ts
+++ dist/Prostgles.d.ts
@@ -143,9 +143,11 @@ export declare type Joins = Join[];
 export declare type ProstglesInitOptions = {
     dbConnection: DbConnection;
     dbOptions?: DbConnectionOpts;
-    publishMethods?: ((params: RequestParams) => {
+    publishMethods?: ((socket?: any, dbo?: DbHandler) => {
         [key: string]: Method;
-    });
+    } | Promise<{
+        [key: string]: Method;
+    }>);
     tsGeneratedTypesDir?: string;
     io?: any;
     publish?: Publish;
