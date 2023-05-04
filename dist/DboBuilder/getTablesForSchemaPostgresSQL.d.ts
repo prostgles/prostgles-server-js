52ed842773335616db43d1cfaca1b6b5ab873f54
root <prostgles@protonmail.com>
2023-05-04 19:56:52 +0000

    fix multi fkey col schema duplication bug

---
+++ dist/DboBuilder/getTablesForSchemaPostgresSQL.d.ts
@@ -0,0 +1,3 @@
+import { DboBuilder, TableSchema } from "../DboBuilder";
+export declare function getTablesForSchemaPostgresSQL({ db, runSQL }: DboBuilder, schema?: string): Promise<TableSchema[]>;
+//# sourceMappingURL=getTablesForSchemaPostgresSQL.d.ts.map
