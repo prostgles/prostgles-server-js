c5acc1e316db506042d8c9effeea28353ce310fa
prostgles <prostgles@protonmail.com>
2023-07-20 19:17:34 +0000

    fix no col tables missing

--- lib/DboBuilder/getTablesForSchemaPostgresSQL.ts
+++ lib/DboBuilder/getTablesForSchemaPostgresSQL.ts
@@ -7,135 +7,135 @@ export async function getTablesForSchemaPostgresSQL({ db, runSQL }: DboBuilder, 
   const query =
     `
     SELECT 
-      jsonb_build_object(
-        'insert', TRUE,
-        'select', TRUE,
-        'update', TRUE,
-        'delete', EXISTS (
-            SELECT 1 
-            FROM information_schema.role_table_grants rg
-            WHERE rg.table_name = t.table_name
-            AND rg.privilege_type = 'DELETE'
-        )
-      ) as privileges
-      , t.table_schema as schema, t.table_name as name 
-      , cc.columns 
-      , cc.table_oid as oid
-      , t.is_view 
-      , t.view_definition 
-      , array_to_json(vr.table_names) as parent_tables
-      , obj_description(cc.table_oid::regclass) as comment
-    FROM ( 
-      SELECT table_name,
-        table_schema,
-        is_view,
-        CASE WHEN is_view THEN pg_get_viewdef(format('%I.%I', table_schema, table_name)::REGCLASS, true) END as view_definition 
-      FROM (
-        SELECT table_name, table_schema, table_type = 'VIEW' as is_view
-        FROM information_schema.tables 
+    jsonb_build_object(
+      'insert', TRUE,
+      'select', TRUE,
+      'update', TRUE,
+      'delete', EXISTS (
+          SELECT 1 
+          FROM information_schema.role_table_grants rg
+          WHERE rg.table_name = t.table_name
+          AND rg.privilege_type = 'DELETE'
+      )
+    ) as privileges
+    , t.table_schema as schema, t.table_name as name 
+    , COALESCE(cc.columns, '[]'::JSONB) as columns 
+    , t.oid
+    , t.is_view 
+    , t.view_definition 
+    , array_to_json(vr.table_names) as parent_tables
+    , obj_description(t.oid::regclass) as comment
+  FROM ( 
+    SELECT table_name,
+      table_schema,
+      oid,
+      is_view,
+      CASE WHEN is_view THEN pg_get_viewdef(format('%I.%I', table_schema, table_name)::REGCLASS, true) END as view_definition 
+    FROM (
+      SELECT table_name, table_schema, table_type = 'VIEW' as is_view, regclass(table_schema || '.' || table_name)::oid as oid
+      FROM information_schema.tables 
 /* TODO - add support for materialized views
-        UNION ALL 
-        SELECT table_name, table_schema
-        FROM (
-          SELECT relname as table_name, nspname as table_schema, true as is_view
-          FROM pg_catalog.pg_class AS _c
-          JOIN pg_catalog.pg_namespace AS _ns
-            ON _c.relnamespace = _ns.oid 
-          WHERE relkind IN ( 'm' )
-        ) materialized_views
+      UNION ALL 
+      SELECT table_name, table_schema
+      FROM (
+        SELECT relname as table_name, nspname as table_schema, true as is_view
+        FROM pg_catalog.pg_class AS _c
+        JOIN pg_catalog.pg_namespace AS _ns
+          ON _c.relnamespace = _ns.oid 
+        WHERE relkind IN ( 'm' )
+      ) materialized_views
 */
-      ) tables_matviews
-      WHERE table_schema = ${asValue(schema)}
-    ) t  
-    INNER  join (
-        SELECT ccc.table_oid, table_schema, table_name
-        , jsonb_agg((SELECT x FROM (
-            SELECT ccc.column_name as name, 
-            ccc.data_type, 
-            ccc.udt_name, 
-            ccc.element_type,
-            ccc.element_udt_name,
-            ccc.is_pkey, 
-            ccc.comment, 
-            ccc.ordinal_position, 
-            ccc.is_nullable = 'YES' as is_nullable,
-            ccc.is_updatable,
-            ccc.references,
-            ccc.has_default,
-            ccc.column_default,
-            COALESCE(ccc.privileges, '[]'::JSON) as privileges
-        ) as x) ORDER BY ccc.ordinal_position ) as columns 
-        FROM (
-            SELECT  c.table_schema, c.table_name, c.column_name, c.data_type, c.udt_name
-            , e.data_type as element_type
-            , e.udt_name as element_udt_name
-            ,  col_description(format('%I.%I', c.table_schema, c.table_name)::regclass::oid, c.ordinal_position) as comment
-            --, CASE WHEN fc.ftable IS NOT NULL THEN row_to_json((SELECT t FROM (SELECT fc.ftable, fc.fcols, fc.cols) t)) END as references
-            , fc.references
-            , c.is_identity = 'YES' OR EXISTS ( 
-                SELECT 1    
-                FROM information_schema.table_constraints as tc 
-                JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name AND tc.table_schema = kcu.table_schema  
-                WHERE kcu.table_schema = c.table_schema AND kcu.table_name = c.table_name AND kcu.column_name = c.column_name AND tc.constraint_type IN ('PRIMARY KEY') 
-            ) as is_pkey
-            , c.ordinal_position
-            , COALESCE(c.column_default IS NOT NULL OR c.identity_generation = 'ALWAYS', false) as has_default
-            , c.column_default
-            , format('%I.%I', c.table_schema, c.table_name)::regclass::oid AS table_oid
-            , c.is_nullable
-              /* generated always and view columns cannot be updated */
-            , COALESCE(c.is_updatable, 'YES') = 'YES' AND COALESCE(c.is_generated, '') != 'ALWAYS' AND COALESCE(c.identity_generation, '') != 'ALWAYS' as is_updatable
-            , cp.privileges
-            FROM information_schema.columns c    
-            LEFT JOIN (SELECT * FROM information_schema.element_types )   e  
-                ON ((c.table_catalog, c.table_schema, c.table_name, 'TABLE', c.dtd_identifier)  
-                = (e.object_catalog, e.object_schema, e.object_name, e.object_type, e.collection_type_identifier)
-            )
-            LEFT JOIN (
-                SELECT table_schema, table_name, column_name, json_agg(row_to_json((SELECT t FROM (SELECT cpp.privilege_type, cpp.is_grantable ) t))) as privileges
-                FROM information_schema.column_privileges cpp
-                GROUP BY table_schema, table_name, column_name
-            ) cp
-                ON c.table_name = cp.table_name AND c.column_name = cp.column_name
-            LEFT JOIN (
-                --SELECT *
-                SELECT "table", unnest(ft.cols) as col, jsonb_agg(row_to_json((SELECT t FROM (SELECT ftable, fcols, cols) t))) as references
-                FROM (
-                  SELECT 
-                    (SELECT r.relname from pg_class r where r.oid = c.conrelid) as table, 
-                    (SELECT array_agg(attname::text) from pg_attribute 
-                    where attrelid = c.conrelid and ARRAY[attnum] <@ c.conkey) as cols, 
-                    (SELECT array_agg(attname::text) from pg_attribute 
-                    where attrelid = c.confrelid and ARRAY[attnum] <@ c.confkey) as fcols, 
-                    (SELECT r.relname from pg_class r where r.oid = c.confrelid) as ftable
-                  FROM pg_constraint c 
-                ) ft
-                WHERE ft.table IS NOT NULL 
-                AND ft.ftable IS NOT NULL 
-                    --  c.confrelid = 'users'::regclass::oid
-                GROUP BY "table",  unnest(cols)
-            ) fc 
-            ON fc.table = c.table_name
-            AND c.column_name::text = fc.col
-        ) ccc
-        GROUP BY table_oid, table_schema, table_name
-    ) cc  
-    ON t.table_name = cc.table_name  
-    AND t.table_schema = cc.table_schema  
-    LEFT JOIN ( 
-        SELECT cl_r.relname as view_name, array_agg(DISTINCT cl_d.relname) AS table_names 
-        FROM pg_rewrite AS r 
-        JOIN pg_class AS cl_r ON r.ev_class=cl_r.oid 
-        JOIN pg_depend AS d ON r.oid=d.objid 
-        JOIN pg_class AS cl_d ON d.refobjid=cl_d.oid 
-        WHERE cl_d.relkind IN ('r','v') 
-        AND cl_d.relname <> cl_r.relname 
-        GROUP BY cl_r.relname 
-    ) vr 
-    ON t.table_name = vr.view_name 
-    GROUP BY t.table_schema, t.table_name, t.is_view, t.view_definition, vr.table_names , cc.table_oid, cc.columns
-    ORDER BY schema, name
-
+    ) tables_matviews
+    WHERE table_schema = ${asValue(schema)}
+  ) t  
+  LEFT join (
+      SELECT table_schema, table_name
+      , jsonb_agg((SELECT x FROM (
+          SELECT ccc.column_name as name, 
+          ccc.data_type, 
+          ccc.udt_name, 
+          ccc.element_type,
+          ccc.element_udt_name,
+          ccc.is_pkey, 
+          ccc.comment, 
+          ccc.ordinal_position, 
+          ccc.is_nullable = 'YES' as is_nullable,
+          ccc.is_updatable,
+          ccc.references,
+          ccc.has_default,
+          ccc.column_default,
+          COALESCE(ccc.privileges, '[]'::JSON) as privileges
+      ) as x) ORDER BY ccc.ordinal_position ) as columns 
+      FROM (
+          SELECT  c.table_schema, c.table_name, c.column_name, c.data_type, c.udt_name
+          , e.data_type as element_type
+          , e.udt_name as element_udt_name
+          ,  col_description(format('%I.%I', c.table_schema, c.table_name)::regclass::oid, c.ordinal_position) as comment
+          --, CASE WHEN fc.ftable IS NOT NULL THEN row_to_json((SELECT t FROM (SELECT fc.ftable, fc.fcols, fc.cols) t)) END as references
+          , fc.references
+          , c.is_identity = 'YES' OR EXISTS ( 
+              SELECT 1    
+              FROM information_schema.table_constraints as tc 
+              JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name AND tc.table_schema = kcu.table_schema  
+              WHERE kcu.table_schema = c.table_schema AND kcu.table_name = c.table_name AND kcu.column_name = c.column_name AND tc.constraint_type IN ('PRIMARY KEY') 
+          ) as is_pkey
+          , c.ordinal_position
+          , COALESCE(c.column_default IS NOT NULL OR c.identity_generation = 'ALWAYS', false) as has_default
+          , c.column_default
+          , format('%I.%I', c.table_schema, c.table_name)::regclass::oid AS table_oid
+          , c.is_nullable
+            /* generated always and view columns cannot be updated */
+          , COALESCE(c.is_updatable, 'YES') = 'YES' AND COALESCE(c.is_generated, '') != 'ALWAYS' AND COALESCE(c.identity_generation, '') != 'ALWAYS' as is_updatable
+          , cp.privileges
+          FROM information_schema.columns c    
+          LEFT JOIN (SELECT * FROM information_schema.element_types )   e  
+              ON ((c.table_catalog, c.table_schema, c.table_name, 'TABLE', c.dtd_identifier)  
+              = (e.object_catalog, e.object_schema, e.object_name, e.object_type, e.collection_type_identifier)
+          )
+          LEFT JOIN (
+              SELECT table_schema, table_name, column_name, json_agg(row_to_json((SELECT t FROM (SELECT cpp.privilege_type, cpp.is_grantable ) t))) as privileges
+              FROM information_schema.column_privileges cpp
+              GROUP BY table_schema, table_name, column_name
+          ) cp
+              ON c.table_name = cp.table_name AND c.column_name = cp.column_name
+          LEFT JOIN (
+              --SELECT *
+              SELECT "table", unnest(ft.cols) as col, jsonb_agg(row_to_json((SELECT t FROM (SELECT ftable, fcols, cols) t))) as references
+              FROM (
+                SELECT 
+                  (SELECT r.relname from pg_class r where r.oid = c.conrelid) as table, 
+                  (SELECT array_agg(attname::text) from pg_attribute 
+                  where attrelid = c.conrelid and ARRAY[attnum] <@ c.conkey) as cols, 
+                  (SELECT array_agg(attname::text) from pg_attribute 
+                  where attrelid = c.confrelid and ARRAY[attnum] <@ c.confkey) as fcols, 
+                  (SELECT r.relname from pg_class r where r.oid = c.confrelid) as ftable
+                FROM pg_constraint c 
+              ) ft
+              WHERE ft.table IS NOT NULL 
+              AND ft.ftable IS NOT NULL 
+                  --  c.confrelid = 'users'::regclass::oid
+              GROUP BY "table",  unnest(cols)
+          ) fc 
+          ON fc.table = c.table_name
+          AND c.column_name::text = fc.col
+      ) ccc
+      GROUP BY table_schema, table_name
+  ) cc  
+  ON t.table_name = cc.table_name  
+  AND t.table_schema = cc.table_schema  
+  LEFT JOIN ( 
+      SELECT cl_r.relname as view_name, array_agg(DISTINCT cl_d.relname) AS table_names 
+      FROM pg_rewrite AS r 
+      JOIN pg_class AS cl_r ON r.ev_class=cl_r.oid 
+      JOIN pg_depend AS d ON r.oid=d.objid 
+      JOIN pg_class AS cl_d ON d.refobjid=cl_d.oid 
+      WHERE cl_d.relkind IN ('r','v') 
+      AND cl_d.relname <> cl_r.relname 
+      GROUP BY cl_r.relname 
+  ) vr 
+  ON t.table_name = vr.view_name 
+  GROUP BY t.table_schema, t.table_name, t.is_view, t.view_definition, vr.table_names , t.oid, cc.columns
+  ORDER BY schema, name
     `;
     
   let result: TableSchema[] = await db.any(query, { schema });
