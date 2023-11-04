a5ce917d23ea4a271a306d0a2582aad8df14f05a
prostgles <prostgles@protonmail.com>
2023-11-04 11:24:51 +0000

    bump types

--- lib/DboBuilder/ViewHandler/getInfo.ts
+++ lib/DboBuilder/ViewHandler/getInfo.ts
@@ -10,35 +10,7 @@ export async function getInfo(this: ViewHandler, lang?: string, param2?: any, pa
   const p = this.getValidatedRules(tableRules, localParams);
   if (!p.getInfo) throw "Not allowed";
 
-  let has_media: "one" | "many" | undefined = undefined;
-
-  const mediaTable = this.dboBuilder.prostgles?.opts?.fileTable?.tableName;
-
-  if (!this.is_media && mediaTable) {
-    const joinConf = this.dboBuilder.prostgles?.opts?.fileTable?.referencedTables?.[this.name]
-    if (joinConf) {
-      has_media = typeof joinConf === "string" ? joinConf : "one";
-    } else {
-      const jp = this.dboBuilder.getShortestJoinPath(this, mediaTable);
-      if (jp && jp.path.length <= 3) {
-        if (jp.path.length <= 2) {
-          has_media = "one"
-        } else {
-          await Promise.all(jp.path.map(async tableName => {
-            const pkeyFcols = this?.dboBuilder?.dbo?.[tableName]?.columns?.filter(c => c.is_pkey).map(c => c.name);
-            const cols = this?.dboBuilder?.dbo?.[tableName]?.columns?.filter(c => c?.references?.some(({ ftable }) => jp.path.includes(ftable)));
-            if (cols && cols.length && has_media !== "many") {
-              if (cols.some(c => !pkeyFcols?.includes(c.name))) {
-                has_media = "many"
-              } else {
-                has_media = "one"
-              }
-            }
-          }));
-        }
-      }
-    }
-  }
+  const fileTableName = this.dboBuilder.prostgles?.opts?.fileTable?.tableName;
 
   return {
     oid: this.tableOrViewInfo.oid,
@@ -48,7 +20,8 @@ export async function getInfo(this: ViewHandler, lang?: string, param2?: any, pa
       allowedNestedInserts: tableRules?.insert?.allowedNestedInserts
     },
     isView: this.is_view,
-    fileTableName: mediaTable,
+    hasFiles: Boolean(!this.is_media && fileTableName && this.columns.some(c => c.references?.some(r => r.ftable === fileTableName))),
+    fileTableName,
     dynamicRules: {
       update: Boolean(tableRules?.update?.dynamicFields?.length)
     }
