ddd8f703b55f3cfa5c24c4df6172d9b7a118a0dc
prostgles <prostgles@protonmail.com>
2024-01-23 10:47:51 +0200

    add temporary test fail

--- tests/serverOnlyQueries.spec.ts
+++ tests/serverOnlyQueries.spec.ts
@@ -28,26 +28,4 @@ export const serverOnlyQueries = async (db: DBHandlerServer) => {
       }
     });
   });
-
-  // // /** Self reference recursion bug */
-  // await db.rec.findOne!({ id: 1 }, { select: { "*": 1, rec_ref: "*" } })
-
-  // /* Transaction example */
-  // await db.tx!(async t => {
-  //   await t.items.insert!({ name: "tx_" });
-  //   const expect1 = await t.items.count!({ name: "tx_" });
-  //   const expect0 = await db.items.count!({ name: "tx_" });
-  //   if(expect0 !== 0 || expect1 !== 1) throw "db.tx failed";
-
-  //   //throw "err"; // Any errors will revert all data-changing commands using the transaction object ( t )
-  // });
-  // const expect1 = await db.items.count!({ name: "tx_" });
-  // if(expect1 !== 1) throw "db.tx failed";
-
-  // /** TableConfig onMount works */
-  // await db.api_table.findOne!({ id: 1 });
-  // const newRow = await db.api_table.insert!({ }, { returning: "*" });
-  // if(newRow.col1 !== null) {
-  //   throw "api_table onMount failed: col1 missing. Got: " + JSON.stringify(newRow);
-  // }
 }
